b8d5eaea33e8546a81f3489420e1442fb60b7fa9
jsko <x1xyz2@gmail.com>
2020-04-24 11:44:33 +0900

    First Homework Final!

--- day1/ascii_game/src/main.rs
+++ day1/ascii_game/src/main.rs
@@ -4,27 +4,23 @@ use ncurses::*;
 fn main() {
     let secret_number: u8 = rand::thread_rng().gen_range(0, 128);
     let secret_char: char = secret_number as char;
-    println!("secret_number {}, secret_char '{}'", secret_number, secret_char);
 
-    let mut try_count = 1;
     initscr();
     refresh();
 
-    println!("secret_number {}, secret_char '{}'\r\n", secret_number, secret_char);
+    println!("secret '{}', ascii-code {}\r\n", secret_char, secret_number);
     loop {
         addstr("Let's try : ");
         let choice_num = getch() as u8;
-        //let choice_char = choice_num as char;
-        try_count = try_count + 1;
    
         if secret_number > choice_num {
-            addstr("\nUp!!\n");
+            addstr("\nLow!!\n");
         }
         else if secret_number < choice_num {
-            addstr("\nDown!!\n");
+            addstr("\nHigh!!\n");
         }
         else {
-            addstr("\nYou're Right!!!, Press anykey to Exit");
+            addstr("\nGood Job!!!, Press anykey to Exit");
             getch();
             break;
         }
